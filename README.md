788c5e0a0d8df4ad78bd5ea26b7d2006c661a83d
Fares Hassen <fares.hassen512@gmail.com>
2023-01-09 23:07:49 +0100

    initialized the files

---
+++ README.md
@@ -0,0 +1,2 @@
+Sorting Visualizer for famous path finding algorithms.
+includes adding path blocks and choosing starting point 
---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from tkinter import *
+import time
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
